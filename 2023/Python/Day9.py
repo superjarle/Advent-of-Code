667e2bf456479d83a9d505cb1e0dbd767d784b8f
Jarle Kvile <jkv@ssb.no>
2023-12-09 16:05:52 +0100

    Tried adding a profiler for learning a cleaner python code

--- 2023/Python/Day9.py
+++ 2023/Python/Day9.py
@@ -1,3 +1,5 @@
+import time
+
 def extrapolate_value(history):
     """
     Extrapolartes the next value and evalute the diff sequence. Returns extrapolated value. 
@@ -9,18 +11,28 @@ def extrapolate_value(history):
         sequences[i].append(sequences[i][-1] + sequences[i + 1][-1])
     return sequences[0][-1]
 
-# Calculates the sum of next and previous values
-def sum_of_next(puzzleinput):
+def profiler(method):
+    def wrapper_method(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = method(*args, **kwargs)
+        elapsed_time = time.perf_counter() - start_time
+        print(f"Method {method.__name__} took: {elapsed_time:.5f} sec")
+        return result
+    return wrapper_method
+
+@profiler
+def part1(puzzleinput):
     with open(puzzleinput, 'r') as file:
         histories = [list(map(int, line.split())) for line in file.read().splitlines()]
     return sum(extrapolate_value(history) for history in histories)
 
-def sum_of_previous(puzzleinput):
+@profiler
+def part2(puzzleinput):
     with open(puzzleinput, 'r') as file:
         histories = [list(map(int, line.split())) for line in file.read().splitlines()]
     return sum(extrapolate_value(history[::-1]) for history in histories)
 
 if __name__ == "__main__":
     puzzleinput = "day9.txt"
-    print("Solution to part 1 is:", sum_of_next(puzzleinput))
-    print("Solution to part 2 is:", sum_of_previous(puzzleinput))
+    print("Solution to part 1 is:", part1(puzzleinput))
+    print("Solution to part 2 is:", part2(puzzleinput))
